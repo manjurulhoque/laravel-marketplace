cc67d25d770a6e67525016c3da9481c0fff95a8a
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-02-23 15:25:03 +0600

    gig model, controller and migrate

---
+++ database/migrations/2018_02_23_091854_create_gigs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGigsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gigs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('title');
+            $table->string('category');
+            $table->text('description');
+            $table->integer('price')->default(5);
+            $table->string('image')->nullable();
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gigs');
+    }
+}
