ccfc528c0cd180882774e146673079028f6fd172
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-03-12 16:54:34 +0600

    show all gigs to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,22 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Gig;
 use App\Slider;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     protected $slider;
+    protected $gig;
 
-    public function __construct(Slider $slider)
+    public function __construct(Slider $slider, Gig $gig)
     {
         $this->slider = $slider;
+        $this->gig = $gig;
     }
 
     public function index()
     {
         $sliders = $this->slider::all();
+        $gigs = $this->gig::where('status', 1)->get();
 
-        return view('index', compact('sliders'));
+        return view('index', compact(['sliders', 'gigs']));
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,3 +2,24 @@
 @section('slider')
     @include('partials.slider', ['sliders' => $sliders])
 @endsection
+@section('content')
+    <div class="container">
+        <div class="row">
+            @foreach ($gigs as $gig)
+                <div class="col-md-3">
+                    <div class="thumbnail">
+                        <a href="{{ route('gigs.show', $gig->id) }}">
+                            <img src="{{asset('/gigs/img/' . $gig->image)}}">
+                        </a>
+                        <div class="caption">
+                            <p><a href="{{ route('gigs.show', $gig->id) }}">{{ $gig->title }}</a></p>
+                            <p><span>by <a href="">{{ $gig->user->name }}</a></span>
+                                <b class="green pull-right">STARTING AT ${{ $gig->price }}</b>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
